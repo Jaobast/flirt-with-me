1d0137da1306556e9163076a2b5722ced28ebc8c
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-05-03 21:04:30 +0200

    bonn

--- script/script.js
+++ script/script.js
@@ -24,8 +24,11 @@ function noFlirt(){
   setTimeout(function(){appendUserMessage("Nope!");}, 0);
   setTimeout(function(){appendUserMessage("ich bin mir nicht sicher");}, 1000);
   setTimeout(function(){appendUserImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGx0ZzV2am4xaXJ0eDYzaGY0aTV3N3ByOGk3bWVpMjFmaW1wY3ZsYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L2Ts72zLIcTWujzCxk/giphy.gif");}, 2000);
-  setTimeout (function(){showNextImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDM4M210NDUwazNyZzlpZ3RhM296dmZ2aG1jN3gzZWkycTBrb2pwMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RiEqMWo9BXzkBAuAhh/giphy.gif", 2000);}, 3000);
-  setTimeout (function(){showNextMessage("Excuse me?", 1000);}, 6000);
+  setTimeout (function(){showNextImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDM4M210NDUwazNyZzlpZ3RhM296dmZ2aG1jN3gzZWkycTBrb2pwMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RiEqMWo9BXzkBAuAhh/giphy.gif", 2000);}, 4000);
+  setTimeout (function(){showNextMessage("Excuse me?", 1000);}, 7000);
+  setTimeout (function(){showNextMessage("Ich werde nochnmal fragen", 1500);}, 9000);
+  setTimeout (function(){showNextMessage("und jetzt gebe ich dir nur eine Auswahl", 1500);}, 11500);
+  setTimeout (function(){showNextMessage("😇", 500);}, 13500);
 }
 
 
