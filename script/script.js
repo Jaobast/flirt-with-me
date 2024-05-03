cd3fb033d84376bee2bd1a809edc0c8ffc97061e
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-05-03 21:08:54 +0200

    text korretur

--- script/script.js
+++ script/script.js
@@ -25,9 +25,9 @@ function noFlirt(){
   setTimeout(function(){appendUserMessage("ich bin mir nicht sicher");}, 1000);
   setTimeout(function(){appendUserImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGx0ZzV2am4xaXJ0eDYzaGY0aTV3N3ByOGk3bWVpMjFmaW1wY3ZsYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L2Ts72zLIcTWujzCxk/giphy.gif");}, 2000);
   setTimeout (function(){showNextImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDM4M210NDUwazNyZzlpZ3RhM296dmZ2aG1jN3gzZWkycTBrb2pwMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RiEqMWo9BXzkBAuAhh/giphy.gif", 2000);}, 4000);
-  setTimeout (function(){showNextMessage("Excuse me?", 1000);}, 7000);
-  setTimeout (function(){showNextMessage("Ich werde nochnmal fragen", 1500);}, 9000);
-  setTimeout (function(){showNextMessage("und jetzt gebe ich dir nur eine Auswahl", 1500);}, 11500);
+  setTimeout (function(){showNextMessage("excuse me?", 1000);}, 7000);
+  setTimeout (function(){showNextMessage("ich frage nochmal", 1500);}, 9000);
+  setTimeout (function(){showNextMessage("und jetzt bekommst du nur eine Auswahl", 1500);}, 11500);
   setTimeout (function(){showNextMessage("😇", 500);}, 13500);
 }
 
