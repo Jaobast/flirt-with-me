3a3fe07f457662e0437cfaedc583c81fd849f47e
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-05-07 12:30:47 +0200

    teste horizontal bildschirm

--- script/script.js
+++ script/script.js
@@ -23,11 +23,11 @@ function noFlirt(){
   setTimeout(function(){appendUserMessage("ich bin mir nicht sicher");}, 1000);
   setTimeout(function(){appendUserImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGx0ZzV2am4xaXJ0eDYzaGY0aTV3N3ByOGk3bWVpMjFmaW1wY3ZsYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L2Ts72zLIcTWujzCxk/giphy.gif");}, 2000);
   setTimeout (function(){showNextImg("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDM4M210NDUwazNyZzlpZ3RhM296dmZ2aG1jN3gzZWkycTBrb2pwMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RiEqMWo9BXzkBAuAhh/giphy.gif", 2000);}, 4000);
-  setTimeout (function(){showNextMessage("excuse me?", 1000);}, 7000);
-  setTimeout (function(){showNextMessage("ich frage nochmal", 1500);}, 9000);
-  setTimeout (function(){showNextMessage("und du bekommst wieder zwei Auswahl", 1500);}, 11500);
+  setTimeout (function(){showNextMessage("excuse me?", 500);}, 7000);
+  setTimeout (function(){showNextMessage("ich frage nochmal", 1500);}, 8500);
+  setTimeout (function(){showNextMessage("und du bekommst wieder zwei Auswahl", 1500);}, 11000);
   setTimeout (function(){showNextMessage("😇", 500);}, 13500);
-  setTimeout (function(){showNextMessage("Erlaubst du mir, mit dir zu flirten? 🙃", 2000);}, 15000);
+  setTimeout (function(){showNextMessage("Erlaubst du mir, mit dir zu flirten? 🙃", 2000);}, 14500);
 setTimeout (function(){
   const div = document.querySelector(".user-input");
         
@@ -41,7 +41,7 @@ setTimeout (function(){
 
   btnJa.addEventListener("click", geschlecht);
   btnKlar.addEventListener("click", geschlecht);
-}, 18000);
+}, 17500);
 }
 
 
